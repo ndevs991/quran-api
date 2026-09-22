e91fda8bcc023a431fd8f0d732cc6b0b20fb7053
timhore991 <162450169+timhore991@users.noreply.github.com>
2026-09-23 05:57:28 +0700

    Update random.js

--- api/random.js
+++ api/random.js
@@ -3,13 +3,13 @@ import quran from "../quran.json";
 export default function handler(req, res) {
     const surah = quran[Math.floor(Math.random() * quran.length)];
 
-    if (!surah.ayah || !surah.ayah.length) {
+    if (!surah.ayahs || !surah.ayahs.length) {
         return res.status(500).json({
-            error: "Surah has no ayah data"
+            error: "Surah has no ayahs data"
         });
     }
 
-    const ayah = surah.ayah[Math.floor(Math.random() * surah.ayah.length)];
+    const ayah = surah.ayahs[Math.floor(Math.random() * surah.ayahs.length)];
 
     res.status(200).json({
         number: surah.number,
